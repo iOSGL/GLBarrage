7044b5cf735cf2edd49d260ece5d52cd64fd9226
GL <1247759034@qq.com>
2016-06-27 16:26:26 +0800

    fix small

--- GLBarrage/ViewController.m
+++ GLBarrage/ViewController.m
@@ -14,8 +14,6 @@
 
 @implementation ViewController
 
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -24,13 +22,13 @@
     if ([self respondsToSelector:@selector(setAutomaticallyAdjustsScrollViewInsets:)]) {
         self.automaticallyAdjustsScrollViewInsets = NO;
     }
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 
 @end

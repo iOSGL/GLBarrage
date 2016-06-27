3d7717378c86e589e14cd94e15f4f8105e6c1208
GL <1247759034@qq.com>
2016-06-27 18:44:19 +0800

    add tableView

--- GLBarrage/Barrage/BarrageView.m
+++ GLBarrage/Barrage/BarrageView.m
@@ -8,6 +8,14 @@
 
 #import "BarrageView.h"
 
+@interface BarrageView () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@property (nonatomic, copy) NSArray *dataArray;
+
+@end
+
 @implementation BarrageView
 
 /*
@@ -21,9 +29,53 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor orangeColor];
+        __weak BarrageView *wself = self;
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            [wself createData];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [wself configUI];
+            });
+        });
+
     }
     return self;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.dataArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *identifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    cell.textLabel.text = self.dataArray[indexPath.row];
+    return cell;
+}
+
+#pragma mark - Private Method
+
+- (void)configUI {
+    [self addSubview:self.tableView];
+}
+
+- (void)createData {
+    self.dataArray = @[@"1", @"2", @"3", @"4", @"5"];
+}
+
+#pragma mark - Setter Getter 
+
+- (UITableView *)tableView {
+    if (_tableView == nil) {
+        _tableView = [[UITableView alloc]initWithFrame:self.bounds style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+    }
+    return _tableView;
 }
 
 @end
